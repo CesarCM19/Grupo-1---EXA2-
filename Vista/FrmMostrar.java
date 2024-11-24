a439aebad03ad21fb9737d7d2cdea41987d801ac
BryanCalderon25 <BRYAN.CALDERONESPINOZA@UCR.AC.CR>
2024-11-24 01:24:15 -0600

    Bryan Calderon Espinoza // Avance de la clase mostrar

--- Vista/FrmMostrar.java
+++ Vista/FrmMostrar.java
@@ -3,6 +3,9 @@ package Vista;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class FrmMostrar {
     private Jtable tablaUsuarios;
@@ -26,6 +29,21 @@ public class FrmMostrar {
         JLabel imagenFondo = new JLabel(fondo);
         imagenFondo.setSize(fondo.getIconWidth(), fondo.getIconHeight());
 
+          // Título centrado
+        JLabel etiquetaTitulo = new JLabel("Usuarios Actuales");
+        etiquetaTitulo.setFont(new Font("Arial", Font.BOLD, 20));
+        etiquetaTitulo.setBounds(355, 20, 250, 40);
+        etiquetaTitulo.setForeground(new Color(38, 104, 184));
+        cuadro.add(etiquetaTitulo);
+
+        // Configuración de la tabla
+        String[] columnas = {"ID", "Primer Nombre", "Segundo Nombre", "Primer Apellido", "Segundo Apellido", "Login", "Clave"};
+        modeloTabla = new DefaultTableModel(columnas, 0);
+        tablaUsuarios = new JTable(modeloTabla);
+        JScrollPane scrollPane = new JScrollPane(tablaUsuarios);
+        scrollPane.setBounds(50, 80, 800, 250);
+        cuadro.add(scrollPane);
     }
+    
     
 }
