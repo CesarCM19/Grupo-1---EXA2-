24ab616b0169c6c2c39d55b886a1489e47490cdb
BryanCalderon25 <BRYAN.CALDERONESPINOZA@UCR.AC.CR>
2024-11-24 00:51:30 -0600

    Bryan JoSue Calderon Espinoza / Creacion de la clase de conexion para mayor facilidad de conectar con la bd e imagenes

---
+++ Modelo/Conexion.java
@@ -0,0 +1,27 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+          // Datos de conexión a la base de datos
+
+    private static final String URL = "jdbc:mysql://localhost:3306/github";
+    private static final String USUARIO = "root";
+    private static final String CONTRASEÑA = "12345";
+
+        // Método para establecer la conexión a la base de datos
+
+    public static Connection conectar() {
+        try {
+            Connection conexion = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
+            System.out.println("Conexión exitosa a la base de datos.");
+            return conexion;
+        } catch (SQLException e) {
+            System.err.println("Error de conexión: " + e.getMessage());
+            return null;
+        }
+    }
+
+}
